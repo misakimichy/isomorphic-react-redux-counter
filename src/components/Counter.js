fccc22757d54f143940fb44ec149d06a780d70c0
Misaki Koonce <misaki.koonce@gmail.com>
2020-03-04 16:37:57 -0800

    Add Counter component with unit test.

---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Counter extends Component {
+    incrementIfOdd = () => {
+        if(this.props.value % 2 !== 0) {
+            this.props.onIncrement()
+        }
+    }
+
+    incrementAsync = () => {
+        setTimeout(this.props.onIncrement, 1000)
+    }
+
+    render() {
+        const { value, onIncrement, onDecrement } = this.props
+        return(
+            <p>
+                Clicked: {value} times
+                {' '}
+                <button onClick={onIncrement}><span role='img' aria-label='plus'>➕</span></button>
+                {' '}
+                <button onClick={onDecrement}><span role='img' aria-label='plus'>➖</span></button>
+                {' '}
+                <button onClick={this.incrementIfOdd}>Increment if odd</button>
+                {' '}
+                <button onClick={this.incrementAsync}>Increment async</button>
+            </p>
+        )
+    }
+
+}
+
+Counter.propTypes = {
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
